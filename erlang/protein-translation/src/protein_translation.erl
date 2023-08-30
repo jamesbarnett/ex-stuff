eb68cbb4e6b991c0a222c3f171e9afe1bfcd8f56
Jim Barnett <james.barnett.jr@gmail.com>
2023-08-29 22:11:11 -0500

    The right version...

--- erlang/protein-translation/src/protein_translation.erl
+++ erlang/protein-translation/src/protein_translation.erl
@@ -2,46 +2,76 @@
 
 -export([proteins/1]).
 
+chunk_n([], _) ->
+    [];
+chunk_n(List, Len) when Len > length(List) ->
+    [List];
+chunk_n(List, Len) ->
+    {Head, Tail} = lists:split(Len, List),
+    [Head | chunk_n(Tail, Len)].
 
-chunk([]) -> [];
-% chunk([C1,C2,C3|[]]) -> [C1,C2,C3];
-chunk([C1,C2,C3|Rest]) ->
-  IsStopToken = lists:member(string:join([C1,C2,C3], ""), ["UAA", "UAG", "UGA"]),
-  if IsStopToken ->
-       [];
-  true ->
-      string:join([C1,C2,C3], "") ++ chunk(Rest)
-  end.
+decode("AUG") ->
+    methionine;
+decode("UUU") ->
+    phenylalanine;
+decode("UUC") ->
+    phenylalanine;
+decode("UUA") ->
+    leucine;
+decode("UUG") ->
+    leucine;
+decode("UCU") ->
+    serine;
+decode("UCC") ->
+    serine;
+decode("UCA") ->
+    serine;
+decode("UCG") ->
+    serine;
+decode("UAU") ->
+    tyrosine;
+decode("UAC") ->
+    tyrosine;
+decode("UGU") ->
+    cysteine;
+decode("UGC") ->
+    cysteine;
+decode("UGG") ->
+    tryptophan;
+decode("UAA") ->
+    stop;
+decode("UAG") ->
+    stop;
+decode("UGA") ->
+    stop;
+decode([_, _, _]) ->
+    nil;
+decode([_, _]) ->
+    nil;
+decode([_]) ->
+    nil;
+decode([]) ->
+    nil.
 
-decode("AUG") -> methionine;
-decode("UUU") -> phenylalanine;
-decode("UUC") -> phenylalanine;
-decode("UUA") -> leucine;
-decode("UUG") -> leucine;
-decode("UCU") -> serine;
-decode("UCC") -> serine;
-decode("UCA") -> serine;
-decode("UCG") -> serine;
-decode("UAU") -> tyrosine;
-decode("UAC") -> tyrosine;
-decode("UGU") -> cysteine;
-decode("UGC") -> cysteine;
-decode("UGG") -> tryptophan;
-decode("UAA") -> stop;
-decode("UAG") -> stop;
-decode("UGA") -> stop;
-decode("") -> erlang:error(badarg);
-decode(Strand) when length(Strand) > 3 ->
-    lists:foreach(fun (X) -> io:format("chunk: ~p~n", X) end, chunk(Strand)),
-    lists:flatten(lists:map(fun (X) -> decode(X) end, chunk(Strand))).
-
-proteins([]) -> {ok, []};
-proteins("UAA") -> {ok, []};
-proteins("UAG") -> {ok, []};
-proteins("UGA") -> {ok, []};
+proteins([]) ->
+    {ok, []};
+proteins("UAA") ->
+    {ok, []};
+proteins("UAG") ->
+    {ok, []};
+proteins("UGA") ->
+    {ok, []};
 proteins(Strand) when length(Strand) == 3 ->
- 	{ok, [decode(Strand)] };
+    case decode(Strand) of
+        Xs when is_atom(Xs) andalso Xs =/= stop andalso Xs =/= nil -> {ok, [Xs]};
+        nil -> {error, badarg}
+    end;
 proteins(Strand) ->
-  Lst = lists:filter(fun (X) -> X =/= stop end, decode(Strand)),
-	Proteins = lists:flatten(Lst),
-	{ok, Proteins }.
+    Chunks = chunk_n(Strand, 3),
+    Proteins = lists:takewhile(
+        fun(X) -> X =/= stop end, lists:map(fun(X) -> decode(X) end, Chunks)
+    ),
+    case lists:member(nil, Proteins) of
+        true -> {error, badarg};
+        false -> {ok, Proteins}
+    end.
