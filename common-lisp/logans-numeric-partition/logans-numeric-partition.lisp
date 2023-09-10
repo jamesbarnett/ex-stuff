92a8c40eda98ade7eab204a3feb5749163259de9
Jim Barnett <james.barnett.jr@gmail.com>
2023-09-10 11:34:13 -0500

    Passing something...

--- common-lisp/logans-numeric-partition/logans-numeric-partition.lisp
+++ common-lisp/logans-numeric-partition/logans-numeric-partition.lisp
@@ -4,6 +4,16 @@
 
 (in-package :logans-numeric-partition)
 
-;; Define categorize-number function
+(defun categorize-number (lst n)
+  (if (oddp n)
+    (reduce #'cons lst :initial-value (list n))
+    (reduce #'cons (cdr lst) :initial-value (cons (car lst) (list n)))))
 
 ;; Define partition-numbers function
+; (defun partition-numbers (lst)
+;   (if (oddp (car lst))
+;     (reduce #'categorize-number (cdr lst) :initial-value (car lst))
+;     (reduce #'categorize-number (cdr lst) :initial-value (car lst)))
+
+(defun partition-numbers (lst)
+  (reduce #'categorize-number (cdr lst) :initial-value (car lst)))
