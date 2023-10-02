f8351a6153925374beedba0b092e926d4e423be9
Jim Barnett <james.barnett.jr@gmail.com>
2023-10-02 00:42:11 -0500

    Adds Common Lisp - Saddle Points exercise

--- common-lisp/saddle-points/saddle-points-test.lisp
+++ common-lisp/saddle-points/saddle-points-test.lisp
@@ -22,44 +22,57 @@
   (and (subsetp alist-one alist-two :test 'equal)
        (subsetp alist-two alist-one :test 'equal)))
 
-(test can-extract-matrix-rows-and-cols
-    (let ((matrix #2A((9 8 7)
-                      (6 6 2)
-                      (3 6 4))))
-      (is (equalp '(6 6 2) (saddle-points:row 2 matrix)))
-      (is (equalp '(9 8 7) (saddle-points:row 1 matrix)))
-      (is (equalp '(3 6 4) (saddle-points:row 3 matrix)))
-      (is (equalp '(9 6 3) (saddle-points:column 1 matrix)))
-      (is (equalp '(8 6 6) (saddle-points:column 2 matrix)))
-      (is (equalp '(7 2 4) (saddle-points:column 3 matrix)))))
+; (test can-adjust-to-stupid-1-based-shit
+;   (is (equal '((1 2) (2 2) (2 3) (3 2)) (saddle-points:increment-points-for-display
+;                                           '((0 1) (1 1) (1 2) (2 1))))))
+;
+; (test can-extract-matrix-rows-and-cols
+;     (let ((matrix #2A((9 8 7)
+;                       (6 6 2)
+;                       (3 6 4))))
+;       (is (equalp '(6 6 2) (saddle-points:row 1 matrix)))
+;       (is (equalp '(9 8 7) (saddle-points:row 0 matrix)))
+;       (is (equalp '(3 6 4) (saddle-points:row 2 matrix)))
+;       (is (equalp '(9 6 3) (saddle-points:column 0 matrix)))
+;       (is (equalp '(8 6 6) (saddle-points:column 1 matrix)))
+;       (is (equalp '(7 2 4) (saddle-points:column 2 matrix)))))
+; (test can-extract-tall-matrix-rows-and-cols
+;     (let ((matrix #2A((9 8)
+;                       (6 6)
+;                       (3 6))))
+;       (is (equalp '(6 6) (saddle-points:row 1 matrix)))
+;       (is (equalp '(9 8) (saddle-points:row 0 matrix)))
+;       (is (equalp '(3 6) (saddle-points:row 2 matrix)))
+;       (is (equalp '(9 6 3) (saddle-points:column 0 matrix)))
+;       (is (equalp '(8 6 6) (saddle-points:column 1 matrix)))))
 
-(test can-find-max-in-a-given-row
-    (let* ((matrix #2A((5 8 7)
-                      (6 6 2)
-                      (3 6 4)))
-           (row-one (saddle-points:row 1 matrix))
-           (col-one (saddle-points:column 1 matrix))
-           (row-two (saddle-points:row 2 matrix)))
-      (is (equalp '((1 2)) (saddle-points:max-positions 1 row-one)))
-      (is (equalp '((3 1)) (saddle-points:min-positions 1 col-one)))
-      (is (equalp '((2 1) (2 2)) (saddle-points:max-positions 2 row-two)))))
-
+; (test can-find-max-in-a-given-row
+;     (let* ((matrix #2A((5 8 7)
+;                       (6 6 2)
+;                       (3 6 4)))
+;            (row-one (saddle-points:row 0 matrix))
+;            (col-one (saddle-points:column 0 matrix))
+;            (row-two (saddle-points:row 1 matrix)))
+;       (is (equalp '((0 1)) (saddle-points:max-positions 0 row-one)))
+;       (is (equalp '((2 0)) (saddle-points:min-positions 0 col-one)))
+;       (is (equalp '((1 0) (1 1)) (saddle-points:max-positions 1 row-two)))))
+;
 ; (test can-find-max-row-and-min-col-positions
 ;     (let ((matrix #2A((5 8 7)
 ;                       (6 6 2)
 ;                       (3 6 4))))
-;         (is (equalp '((1 2) (2 1) (2 2) (3 1))
-;                     (saddle-points:max-row-positions matrix)))))
-        ; (is (equalp '((3 1) (2 2) (3 2) (2 3))
-        ;             (saddle-points:min-col-positions matrix)))))
-; (test can-identify-single-saddle-point
-;     (let ((matrix #2A((9 8 7)
-;                       (5 3 2)
-;                       (6 6 7)))
-;           (result '((2 1))))
-;       (is (equal-coordinates result (saddle-points:saddle-points matrix)))))
-;
-;
+;         (is (equalp '((0 1) (1 0) (1 1) (2 1))
+;                     (saddle-points:max-row-positions matrix)))
+;         (is (equalp '((2 0) (1 1) (2 1) (1 2))
+;                     (saddle-points:min-col-positions matrix)))))
+(test can-identify-single-saddle-point
+    (let ((matrix #2A((9 8 7)
+                      (5 3 2)
+                      (6 6 7)))
+          (result '((2 1))))
+      (is (equal-coordinates result (saddle-points:saddle-points matrix)))))
+
+
 (test can-identify-that-empty-matrix-has-no-saddle-points
     (let ((matrix #2A(()))
           (result '()))
@@ -72,46 +85,49 @@
           (result '()))
       (is (equal-coordinates result (print (saddle-points:saddle-points matrix))))))
 
-; (test can-identify-multiple-saddle-points-in-a-column
-;     (let ((matrix #2A((4 5 4)
-;                       (3 5 5)
-;                       (1 5 4)))
-;           (result '((1 2) (2 2) (3 2))))
-;       (is (equal-coordinates result (saddle-points:saddle-points matrix)))))
-;
-; (test can-identify-multiple-saddle-points-in-a-row
-;     (let ((matrix #2A((6 7 8)
-;                       (5 5 5)
-;                       (7 5 6)))
-;           (result '((2 1) (2 2) (2 3))))
-;       (is (equal-coordinates result (saddle-points:saddle-points matrix)))))
-;
-; (test can-identify-saddle-point-in-bottom-right-corner
-;     (let ((matrix #2A((8 7 9)
-;                       (6 7 6)
-;                       (3 2 5)))
-;           (result '((3 3))))
-;       (is (equal-coordinates result (saddle-points:saddle-points matrix)))))
-;
+(test can-identify-multiple-saddle-points-in-a-column
+    (let ((matrix #2A((4 5 4)
+                      (3 5 5)
+                      (1 5 4)))
+          (result '((1 2) (2 2) (3 2))))
+      (is (equal-coordinates result (saddle-points:saddle-points matrix)))))
+
+(test can-identify-multiple-saddle-points-in-a-row
+    (let ((matrix #2A((6 7 8)
+                      (5 5 5)
+                      (7 5 6)))
+          (result '((2 1) (2 2) (2 3))))
+      (is (equal-coordinates result (saddle-points:saddle-points matrix)))))
+
+(test can-identify-saddle-point-in-bottom-right-corner
+    (let ((matrix #2A((8 7 9)
+                      (6 7 6)
+                      (3 2 5)))
+          (result '((3 3))))
+      (is (equal-coordinates result (saddle-points:saddle-points matrix)))))
+
 (test can-identify-saddle-points-in-a-non-square-matrix
     (let ((matrix #2A((3 1 3)
                       (3 2 4)))
           (result '((1 3) (1 1))))
+          (format nil "~%~%Matrix (non-square): ~a~%~%" (array-dimensions matrix))
+        ; (max-positions 1
+        ; (min-positions
       (is (equal-coordinates result (saddle-points:saddle-points matrix)))))
 
-; (test can-identify-that-saddle-points-in-a-single-column-matrix-are-those-with-the-minimum-value
-;     (let ((matrix #2A((2)
-;                       (1)
-;                       (4)
-;                       (1)))
-;           (result '((2 1) (4 1))))
-;       (is (equal-coordinates result (saddle-points:saddle-points matrix)))))
-;
-; (test can-identify-that-saddle-points-in-a-single-row-matrix-are-those-with-the-maximum-value
-;     (let ((matrix #2A((2 5 3 5)))
-;           (result '((1 2) (1 4))))
-;       (is (equal-coordinates result (saddle-points:saddle-points matrix)))))
-;
+(test can-identify-that-saddle-points-in-a-single-column-matrix-are-those-with-the-minimum-value
+    (let ((matrix #2A((2)
+                      (1)
+                      (4)
+                      (1)))
+          (result '((2 1) (4 1))))
+      (is (equal-coordinates result (saddle-points:saddle-points matrix)))))
+
+(test can-identify-that-saddle-points-in-a-single-row-matrix-are-those-with-the-maximum-value
+    (let ((matrix #2A((2 5 3 5)))
+          (result '((1 2) (1 4))))
+      (is (equal-coordinates result (saddle-points:saddle-points matrix)))))
+
 (defun run-tests (&optional (test-or-suite 'saddle-points-suite))
   "Provides human readable results of test run. Default to entire suite."
   (run! test-or-suite))
--- common-lisp/saddle-points/saddle-points.lisp
+++ common-lisp/saddle-points/saddle-points.lisp
@@ -1,15 +1,6 @@
 (defpackage :saddle-points
   (:use :cl)
-  (:export :saddle-points
-           :make-range
-           :row
-           :column
-           :max-positions
-           :min-positions
-           :indices-of-max-values
-           :min-col-position
-           :max-row-positions
-           :min-col-positions))
+  (:export :saddle-points))
 
 (in-package :saddle-points)
 
@@ -17,38 +8,40 @@
   (loop for num from 0 below n collect num))
 
 (defun row (row-num matrix)
-  (loop for n below (array-dimension matrix 0)
-      collecting (aref matrix (1- row-num) n) into rows
+  (loop for n below (array-dimension matrix 1)
+      collecting (aref matrix row-num n) into rows
   finally (return rows)))
 
 (defun column (col-num matrix)
-  (loop for n below (array-dimension matrix 1)
-      collecting (aref matrix (1- col-num) n) into cols
+  (loop for n below (array-dimension matrix 0)
+      collecting (aref matrix n col-num) into cols
   finally (return cols)))
 
 (defun positions-for-row (n arr)
-  (mapcar (lambda (x) (list n (1+ x)))
+  (mapcar (lambda (x) (list n x))
           (make-range (length arr))))
 
 (defun positions-for-col (n arr)
-  (mapcar (lambda (x) (list (1+ x) n))
+  (mapcar (lambda (x) (list x n))
           (make-range (length arr))))
 
-(defun max-positions (n arr)
-  (remove-if-not (lambda (x) (eq (nth (1- (cadr x)) arr) (max-array-value arr)))
-                   (positions-for-row n arr)))
+(defun max-positions (n matrix)
+  (let ((arr (row n matrix)))
+    (remove-if-not (lambda (x) (eq (nth (cadr x) arr) (max-array-value arr)))
+                     (positions-for-row n arr))))
 
-(defun min-positions (n arr)
-  (remove-if-not (lambda (x) (eq (nth (1- (car x)) arr) (min-array-value arr)))
-                 (positions-for-col n arr)))
+(defun min-positions (n matrix)
+  (let ((arr (column n matrix)))
+    (remove-if-not (lambda (x) (eq (nth (car x) arr) (min-array-value arr)))
+                   (positions-for-col n arr))))
 
-(defun max-row-positions(matrix)
-  (mapcan (lambda (x) (max-positions (1+ x) (row (1+ x) matrix)))
-          (make-range (array-dimension matrix 0))))
+(defun max-row-positions(dim matrix)
+  (mapcan (lambda (x) (max-positions x matrix))
+          (make-range dim)))
 
-(defun min-col-positions(matrix)
-  (mapcan (lambda (x) (min-positions (1+ x) (column (1+ x) matrix)))
-          (make-range (array-dimension matrix 1))))
+(defun min-col-positions(dim matrix)
+  (mapcan (lambda (x) (min-positions x matrix))
+          (make-range dim)))
 
 (defun max-array-value (arr)
   (apply #'max arr))
@@ -56,9 +49,35 @@
 (defun min-array-value (arr)
   (apply #'min arr))
 
+(defun increment-point (pt)
+  (cons (1+ (car pt)) (cons (1+ (cadr pt)) '())))
+
+(defun increment-points-for-display (sadd-pts)
+  (cond ((null sadd-pts) nil)
+         ((numberp (caar sadd-pts))
+          (cons (increment-point (car sadd-pts))
+                (increment-points-for-display (cdr sadd-pts))))))
+
+(defun rows-in-matrix (matrix)
+  (let ((dims (array-dimensions matrix)))
+    (if (eq 2 (length dims))
+      (car dims)
+      0)))
+
+(defun columns-in-matrix (matrix)
+  (let ((dims (array-dimensions matrix)))
+    (if (eq 2 (length dims))
+       (cadr dims)
+       0)))
+
 (defun saddle-points (matrix)
   (if (some (lambda (x) (= x 0)) (array-dimensions matrix))
     ()
-    (let ((maxes (max-row-positions matrix))
-          (mins (min-col-positions matrix)))
-      (intersection maxes mins :test 'equal))))
+    (let* ((row-count (rows-in-matrix matrix))
+           (column-count (columns-in-matrix matrix))
+           (maxes (max-row-positions row-count matrix))
+           (mins (min-col-positions column-count matrix)))
+      (increment-points-for-display
+        (intersection maxes mins :test 'equal)))))
+
+
